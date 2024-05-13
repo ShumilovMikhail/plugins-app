06dec93f8559411f0bfe81245eee9857b9ede6fb
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-05-13 09:54:59 +0300

    [frontend][feature] add plugin service

--- app/frontend/src/app/pages/page-landing-editor/page-landing-editor.component.ts
+++ app/frontend/src/app/pages/page-landing-editor/page-landing-editor.component.ts
@@ -2,17 +2,20 @@ import { Component, OnInit, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { HeaderService } from '../../services/header.service';
+import { PluginsService } from '../../services/plugins.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-page-landing-editor',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './page-landing-editor.component.html',
 })
 export class PageLandingEditorComponent implements OnInit {
   private readonly headerService = inject(HeaderService);
+  protected readonly pluginsService = inject(PluginsService);
 
   ngOnInit(): void {
     this.headerService.pageName.set('Landing Editor');
-  };
-};
+  }
+}
---
+++ app/frontend/src/app/services/plugins.service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, delay } from "rxjs";
+
+@Injectable({ providedIn: 'root' })
+export class PluginsService {
+  private readonly plugins = new BehaviorSubject([]);
+  public readonly plugins$ = this.plugins.asObservable().pipe(
+    delay(100)
+  );
+};
