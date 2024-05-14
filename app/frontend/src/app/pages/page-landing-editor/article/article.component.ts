ec9e01ef3e40200fa6b27bce753c6cb0e85ef5dc
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-05-14 19:28:47 +0300

    [frontend][fix] fix for displaying content items

--- app/frontend/src/app/pages/page-landing-editor/article/article.component.ts
+++ app/frontend/src/app/pages/page-landing-editor/article/article.component.ts
@@ -1,65 +1,16 @@
-import {
-  AfterViewInit,
-  Component,
-  ComponentRef,
-  Input,
-  Type,
-  ViewChild,
-  ViewContainerRef,
-  inject,
-} from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { combineLatestWith } from 'rxjs';
 
 import { ArticleType } from '../../../types/article.type';
 import { PluginsHostDirective } from './plugins-host.directive';
-import { ArticleService } from '../../../services/article.service';
-import { ContentItem } from '../../../types/contentItem.interface';
-import { PluginsService } from '../../../services/plugins.service';
-import { PluginsMap } from '../../../types/pluginsMap.interface';
-
-interface PluginComponentType {
-  data: any;
-}
+import { RefComponent } from './ref/ref.component';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [CommonModule, PluginsHostDirective],
   templateUrl: './article.component.html',
+  imports: [CommonModule, PluginsHostDirective, RefComponent],
 })
-export class ArticleComponent implements AfterViewInit {
-  @ViewChild(PluginsHostDirective, { static: true })
-  private readonly pluginsHost!: PluginsHostDirective;
+export class ArticleComponent {
   @Input({ required: true }) article!: ArticleType;
-  private readonly articleService = inject(ArticleService);
-  private readonly pluginsService = inject(PluginsService);
-
-  ngAfterViewInit(): void {
-    const container: ViewContainerRef = this.pluginsHost.viewContainerRef;
-    this.articleService.article$
-      .pipe(combineLatestWith(this.pluginsService.plugins$))
-      .subscribe(([article, pluginsMap]: [ArticleType, PluginsMap]) => {
-        container.clear();
-
-        article.forEach((contentItem: ContentItem) => {
-          const foundPlugin = pluginsMap[contentItem.pluginSlug];
-
-          if (foundPlugin) {
-            this.loadComponent(container, foundPlugin.componentType, contentItem.data);
-          }
-        });
-      });
-  }
-
-  private loadComponent(
-    container: ViewContainerRef,
-    component: Type<PluginComponentType>,
-    data: unknown,
-  ) {
-    const componentRef: ComponentRef<PluginComponentType> =
-      container.createComponent<PluginComponentType>(component);
-    componentRef.instance.data = data;
-    componentRef.changeDetectorRef.detectChanges();
-  }
 }
---
+++ app/frontend/src/app/pages/page-landing-editor/article/ref/ref.component.ts
@@ -0,0 +1,54 @@
+import {
+  AfterViewInit,
+  Component,
+  ComponentRef,
+  Input,
+  Type,
+  ViewChild,
+  ViewContainerRef,
+  inject,
+} from '@angular/core';
+
+import { PluginsHostDirective } from '../plugins-host.directive';
+import { PluginsService } from '../../../../services/plugins.service';
+import { PluginsMap } from '../../../../types/pluginsMap.interface';
+import { ContentItem } from '../../../../types/contentItem.interface';
+
+interface PluginComponentType {
+  data: any;
+}
+
+@Component({
+  selector: 'app-ref',
+  standalone: true,
+  imports: [PluginsHostDirective],
+  templateUrl: './ref.component.html',
+})
+export class RefComponent implements AfterViewInit {
+  @Input({ required: true }) contentItem!: ContentItem;
+  @ViewChild(PluginsHostDirective, { static: true })
+  private readonly pluginsHost!: PluginsHostDirective;
+  private readonly pluginsService = inject(PluginsService);
+
+  ngAfterViewInit(): void {
+    const container: ViewContainerRef = this.pluginsHost.viewContainerRef;
+    container.clear();
+    this.pluginsService.plugins$.subscribe((pluginsMap: PluginsMap) => {
+      const foundPlugin = pluginsMap[this.contentItem.pluginSlug];
+      if (foundPlugin) {
+        this.loadComponent(container, foundPlugin.componentType, this.contentItem.data);
+      }
+    });
+  }
+
+  private loadComponent(
+    container: ViewContainerRef,
+    component: Type<PluginComponentType>,
+    data: unknown,
+  ) {
+    const componentRef: ComponentRef<PluginComponentType> =
+      container.createComponent<PluginComponentType>(component);
+    componentRef.instance.data = data;
+    componentRef.changeDetectorRef.detectChanges();
+  }
+}
