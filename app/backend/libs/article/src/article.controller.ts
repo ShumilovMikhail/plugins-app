4d4f7145a1798047c2b6735ba7867e296f3ef385
Mihail Shumilov <misha.shumilov1327@gmail.com>
2024-05-15 18:03:48 +0300

    [backend][feature] add article api

---
+++ app/backend/libs/article/src/article.controller.ts
@@ -0,0 +1,26 @@
+import { readFileSync, existsSync, writeFileSync } from 'fs';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ArticleType } from '@app/types/article.type';
+
+const ARTICLE_PATH = 'article.json';
+
+@Controller('article')
+export class ArticleController {
+  constructor() {
+    if (!existsSync(ARTICLE_PATH)) {
+      writeFileSync(ARTICLE_PATH, '[]');
+    }
+  }
+
+  @Get()
+  readArticle(): ArticleType {
+    return JSON.parse(readFileSync(ARTICLE_PATH, { encoding: 'utf-8' }));
+    // return this.articleService.getHello();
+  }
+
+  @Post()
+  updateArticle(@Body() article: ArticleType): ArticleType {
+    writeFileSync(ARTICLE_PATH, JSON.stringify(article, null, 2));
+    return article;
+  }
+}
